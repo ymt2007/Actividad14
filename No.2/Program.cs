79bfac4c6e31993ee10ec41c6d262bbeaf8a1628
ymt2007 <pm789481@gmail.com>
2026-03-17 09:08:52 -0600

    Lista completa

--- No.2/Program.cs
+++ No.2/Program.cs
@@ -1,6 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-class producto
+
+List<Producto> productos = new List<Producto>();
+Console.WriteLine("Cuantos productos desea ingresar?");
+int n=int.Parse(Console.ReadLine());
+for(int i=0; i<n; i++)
+{
+   Producto P=new Producto();
+    Console.WriteLine($"Producto {i+1}");
+    Console.WriteLine("Nombre del producto: "); P.Nombre = Console.ReadLine();
+    Console.WriteLine("Precio: "); P.Precio=double.Parse(Console.ReadLine());
+    Console.WriteLine("Cantidad: "); P.Cantidad = int.Parse(Console.ReadLine());
+    productos.Add(P);
+}
+Console.Clear();
+double sumaInventario = 0;
+Producto masCaro = productos[0];
+Console.WriteLine("Inventario");
+foreach(Producto p in productos)
+{
+    p.MostrarInformacion();
+    sumaInventario += p.ValorInventario();
+    if (p.Precio > masCaro.Precio) 
+    { 
+        masCaro = p; 
+    }
+}
+Console.WriteLine($"Total del inventario: {sumaInventario:F2}");
+Console.WriteLine("Producto mas caro:");
+masCaro.MostrarInformacion();
+class Producto
 {
     public string Nombre;
     public double Precio;
@@ -11,9 +40,9 @@ class producto
     }
     public string ExisteciaStock()
     {
-        if (Cantidad == 0) return "Si9n existecia";
+        if (Cantidad == 0) return "Sin existecia";
         else if (Cantidad <= 6) return "Stock bajo";
-        else return "Stock suficinte";
+        else return "Stock suficiente";
     }
     public void MostrarInformacion()
     {
