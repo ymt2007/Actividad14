e52bfe2cc85949667e8935d6eb796c6a89c2f862
ymt2007 <pm789481@gmail.com>
2026-03-17 08:53:07 -0600

    creacion de la clase

---
+++ No.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+class producto
+{
+    public string Nombre;
+    public double Precio;
+    public int Cantidad;
+    public double ValorInventario()
+    {
+        return (Precio * Cantidad);
+    }
+    public string ExisteciaStock()
+    {
+        if (Cantidad == 0) return "Si9n existecia";
+        else if (Cantidad <= 6) return "Stock bajo";
+        else return "Stock suficinte";
+    }
+    public void MostrarInformacion()
+    {
+        Console.WriteLine($"Producto: {Nombre} precio: Q{Precio:F2} cantidad {Cantidad} total: Q{ValorInventario()} estado: {ExisteciaStock()} ");
+    }
+}
