59fcf567f0d36eebcb553ca2a5248bc464d22f97
ymt2007 <pm789481@gmail.com>
2026-03-17 08:30:00 -0600

    creacion de clase y Lista

--- No.1/Program.cs
+++ No.1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+List<Estudiante> estudiantes = new List<Estudiante>();
+Console.WriteLine("Cuantos estudiantes desea ingresar?");
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    Estudiante e = new Estudiante();
+    Console.WriteLine($"Estudiante {i + 1}");
+}
+
+class Estudiante
+{
+    public string Nombre;
+    public double Nota1;
+    public double Nota2;
+    public double Nota3;
+    public double CalcularPromedio()
+    {
+        return (Nota1 + Nota2 + Nota3) / 3;
+    }
+    public string Estado()
+    {
+        if (CalcularPromedio() >= 61) return "Aprobado";
+        else return "Reprobado";
+    }
+    public void MostrarInfotmacion()
+    {
+        Console.WriteLine($"Nombre: {Nombre} promedio: {CalcularPromedio:F2} Estado: {Estado}");
+    }
+}
