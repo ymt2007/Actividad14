db4e77aa2e96641b31f7f76e2f47ae6319fcbb3a
Martinez . <pm789481@gmail.com>
2026-03-17 18:44:09 -0600

    creacion d la lista y verificacion

--- No.4/Program.cs
+++ No.4/Program.cs
@@ -1,5 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+List<Libro> libros = new List<Libro>();
+Console.WriteLine("Cuantos libros desea registrar?");
+int n=int.Parse(Console.ReadLine());
+for(int i=0;i<n;i++)
+{
+    Libro l = new Libro();
+    Console.WriteLine($"Libro {i+1}");
+    Console.WriteLine("Titulo: "); l.Titulo = Console.ReadLine();
+    Console.WriteLine("Autor: "); l.Autor = Console.ReadLine();
+    Console.WriteLine("Categoria: "); l.Categoria = Console.ReadLine();
+    Console.WriteLine("Numero de paginas: "); l.Paginas=int.Parse(Console.ReadLine());
+    libros.Add(l);
+}
+Console.Clear();
+Libro mayor = libros[0];
+Console.WriteLine("Libros registrados");
+foreach(Libro l in libros)
+{
+    l.MostrarInformacion();
+    if(l.Paginas>mayor.Paginas)
+    {
+        mayor = l;
+    }
+}
+Console.WriteLine("El libro con mas paginas es: ");
+mayor.MostrarInformacion();
 class Libro
 {
     public string Titulo;
