717a3b10135e33be6fe0f2eadda0315ea7b838a8
Martinez . <pm789481@gmail.com>
2026-03-18 17:41:29 -0600

    lista con retiro y deposito preguntandole si desea hacer una modificacion

--- No.8/Program.cs
+++ No.8/Program.cs
@@ -1,7 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 
-
+List<Cuenta> cuentas=new List<Cuenta>();
+Console.WriteLine("Cuantas cuentas desea ingresar?");
+int n = int.Parse(Console.ReadLine());
+for(int i=0;i<n;i++)
+{
+    Cuenta c=new Cuenta();
+    Console.WriteLine($"Cuenta {i+1}");
+    Console.WriteLine("Titular: "); c.Titular = Console.ReadLine();
+    Console.WriteLine("Saldo: "); c.Saldo=double.Parse(Console.ReadLine());
+    cuentas.Add(c);
+}
+Console.Clear();
+string op;
+int op1=0;
+do
+{
+    Console.WriteLine("Desea hacer una modificacion con la cuenta? (s/n)");
+    op=Console.ReadLine();
+    if( op=="s")
+    {
+        Console.WriteLine("Seleccione la cuenta (en numero)");
+        for(int i=0;i<cuentas.Count;i++)
+        {
+            Console.WriteLine($"{i+1}. {cuentas[i].Titular}");
+        }
+        int indice=int.Parse(Console.ReadLine())-1;
+        Console.WriteLine("1. Deposito");
+        Console.WriteLine("2. Retiro");
+        Console.WriteLine("3. Salir ");
+        Console.WriteLine("Ingrese la opcion que necesita (en numero)");
+        op1 = int.Parse(Console.ReadLine());
+        if (op1 == 1)
+        {
+            cuentas[indice].Depositar();
+        }
+        else if (op1 == 2)
+        {
+            Console.WriteLine(cuentas[indice].Retirar());
+        }
+    }
+} while (op == "s" && op1 != 3); 
+foreach(Cuenta c in cuentas)
+{
+    c.MostrarInformacion();
+}
 
 class Cuenta
 {
