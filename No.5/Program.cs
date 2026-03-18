62296b3edc921f032fdaa51eccbd2bcfdb7aa270
Martinez . <pm789481@gmail.com>
2026-03-17 19:01:03 -0600

    creacion de clase

---
+++ No.5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+class Venta
+{
+    public string Producto;
+    public double Precio;
+    public int Cantidad;
+    public double Subtotal()
+    {
+        return (Precio * Cantidad);
+    }
+    public double Descuento()
+    {
+        if (Subtotal() > 2000) return Subtotal() * 0.10;
+        else if (Subtotal() >= 1000) return Subtotal() * 0.05;
+        else return 0;
+    }
+    public double MontoTotal()
+    {
+        return Subtotal()-Descuento();
+    }
+    public void MostrarInformacion()
+    {
+        Console.WriteLine($"Producto: {Producto} Precio: {Precio} Cantidad; {Cantidad} Subtotal: {Subtotal():F2} Descuento:{Descuento():F2} Total a pagar: {MontoTotal():F2}");
+    }
+}
