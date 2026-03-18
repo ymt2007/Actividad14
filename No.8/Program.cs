edc160c1c0eeb4c2bb998d71aa4fbeaa19987719
Martinez . <pm789481@gmail.com>
2026-03-18 17:14:22 -0600

    creacion de clase

---
+++ No.8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+class Cuenta
+{
+    public string Titular;
+    public double Saldo;
+    public double Depositar()
+    {
+        Console.WriteLine("Cuanto dinero desea depositar?");
+        double monto=double.Parse(Console.ReadLine());
+        Console.WriteLine("Deposito realizado");
+        return Saldo+=monto;
+    }
+    public string Retirar()
+    {
+        Console.WriteLine("Cuanto dinero desea retirar?");
+        double retiro=double.Parse(Console.ReadLine());
+        if (retiro <= 0) return "Cantidad invalida";
+        else if (retiro > Saldo) return "Error, el retiro no puede ser mayor al saldo";
+        else
+        {
+            Saldo-=retiro;
+            return "Retiro realizado con exito!";
+        }
+    }
+    public void MostrarInformacion()
+    {
+        Console.WriteLine($"Titular: {Titular} Saldo: {Saldo }");
+    }
+}
