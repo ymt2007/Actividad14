4d28f19e414ae1670ed3897e89f01f06dd88c416
Martinez . <pm789481@gmail.com>
2026-03-17 18:35:35 -0600

    creacion de clase

--- No.3/Program.cs
+++ No.3/Program.cs
@@ -4,6 +4,21 @@ using System.Collections.Generic;
 List<Empleado> empleados=new List<Empleado>();
 Console.WriteLine("Cuantos empleados desea ingresar?");
 int n=int.Parse(Console.ReadLine());
+for(int i=0;i<n;i++)
+{
+    Empleado e=new Empleado();
+    Console.WriteLine($"Empleado {i+1}");
+    Console.WriteLine("Nombre: "); e.Nombre = Console.ReadLine();
+    Console.WriteLine("Puesto: "); e.Puesto = Console.ReadLine();
+    Console.WriteLine("Salario mensual: "); e.Salario=double.Parse(Console.ReadLine());
+    empleados.Add(e);
+}
+Console.Clear();
+Console.WriteLine("Listado de empleados ");
+foreach(Empleado e in empleados)
+{
+    e.MostrarDatos();
+}
 
 class Empleado
 {
@@ -13,13 +28,13 @@ class Empleado
     public double CalcularSalarioAnual() { return Salario * 12; }
     public double CalcularBono()
     {
-        if (Salario >= 5000) return Salario * 0.15;
-        else return Salario * 0.08;
+        if (Salario >= 6000) return Salario * 0.15;
+        else return Salario * 0.10;
     }
     public string ClasificarSalario()
     {
-        if (Salario >= 7000) return "Salario alto";
-        else if (Salario >= 4000) return "Salario medio";
+        if (Salario >= 8000) return "Salario alto";
+        else if (Salario >= 4500) return "Salario medio";
         else return "Salario básico";
     }
     public void MostrarDatos()
---
+++ No.4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+class Libro
+{
+    public string Titulo;
+    public string Autor;
+    public string Categoria;
+    public int Paginas;
+    public string Clasificacion()
+    {
+     if (Paginas >= 500) return "Muy extenso";
+     else if (Paginas >= 200) return "Mediano";
+     else return "Corto";
+    }
+    public void MostrarInformacion()
+    {
+        Console.WriteLine($"Titulo: {Titulo} Autor: {Autor} categoria: {Categoria} paginas: {Paginas} clasificaion: {Clasificacion()}");
+    }
+}
