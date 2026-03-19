4eb4c92163a55beb237f4d6aa4f05b7684bcc1e4
Martinez . <pm789481@gmail.com>
2026-03-18 18:12:49 -0600

    lista completa

--- No.9/Program.cs
+++ No.9/Program.cs
@@ -9,8 +9,23 @@ for(int i=0; i<n;i++)
     Curso c = new Curso();
     Console.WriteLine($"Curso {i+1}");
     Console.WriteLine("Nombre del curso: "); c.NombreCurso = Console.ReadLine();
-    Console.WriteLine();
+    Console.WriteLine("Creditos: "); c.Creditos=int.Parse((string)Console.ReadLine());
+    Console.WriteLine("Nota promedio: "); c.NotaPromedio = double.Parse(Console.ReadLine());
+    cursos.Add(c);
 }
+Console.Clear();
+Curso mejor = cursos[0];
+Console.WriteLine("Cursos regsitrados: ");
+foreach(Curso c in cursos)
+{
+    c.MostrarInformacion();
+    if(c.NotaPromedio>mejor.NotaPromedio)
+    {
+        mejor = c;
+    }
+}
+Console.WriteLine("Mejor curso: ");
+mejor.MostrarInformacion();
 class Curso
 {
     public string NombreCurso;
@@ -18,12 +33,13 @@ class Curso
     public double NotaPromedio;
     public string Rendimiento()
     {
-        if (NotaPromedio > 0 && NotaPromedio <= 50) return "Bajo";
+        if (NotaPromedio < 0) return "Nota negativa no valida";
+        else if (NotaPromedio > 0 && NotaPromedio <= 50) return "Bajo";
         else if (NotaPromedio <= 70) return "Aceptable";
         else return "Excelente";
     }
     public void MostrarInformacion()
     {
-        Console.WriteLine($"Curso: {NombreCurso} Creditos: {Creditos} Promedio:{NotaPromedio:F2} Rendimiento: {Rendimiento()}");
+        Console.WriteLine($"Curso: {NombreCurso} Creditos: {Creditos} Promedio: {NotaPromedio:F2} Rendimiento: {Rendimiento()}");
     }
 }
